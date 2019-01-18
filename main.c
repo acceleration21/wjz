d0125cb74bad8da6f530376a9da87371f27e55ad
wangjunze <171635094@qq.com>
2019-01-18 10:18:38 +0800

    项目初始化

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+
+    while (1)
+    {
+        printf("1--添加成绩\n");
+        printf("2--删除全部\n");
+        printf("3--显示全部成绩\n");
+        printf("");
+        printf("5--退出\n");
+
+        printf("6--请输入相关的功能编码\n");
+        int code;
+        scanf("%d", &code);
+        
+        if (code == 1) {
+        }
+          if (code == 1) {
+        }
+          if (code == 1) {
+        }
+          if (code == 1) {
+        }
+          if (code == 1) {
+        }
+        
+    }
+
+    return 0;
+}
